377606a2a60e28dc2c1691f6664e8714c7038c97
Aruni Amarasinghe <aruniamarasinghe@Arunis-MacBook-Air.local>
2019-01-20 21:19:59 +0530

    removed unused toast msgs, changed dashboard view

--- app/src/main/java/com/edebtor/oclimb/edebtor/main_menu.java
+++ app/src/main/java/com/edebtor/oclimb/edebtor/main_menu.java
@@ -10,9 +10,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.net.Uri;
+//import com.edebtor.oclimb.edebtor.main_menu.webkit.WebView;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
 
 public class main_menu extends AppCompatActivity {
     Button btn_crtManage,new_loan,new_cus,my_profile,sync,logout,url;
+    private android.webkit.WebView mWebView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +32,24 @@ public class main_menu extends AppCompatActivity {
         //new_loan=findViewById(R.id.new_loan);
         //new_cus=findViewById(R.id.new_cus);
         //my_profile=findViewById(R.id.my_profile);
+        //Button webview = findViewById(R.id.webview1);
+       // mWebView = (android.webkit.WebView)findViewById(R.id.mWebView1);
         url =findViewById(R.id.url);
         sync=findViewById(R.id.sync);
         logout=findViewById(R.id.logout);
+
+        //open website within the app
+//        webview.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                //webView.getSettings().setJavaScriptEnabled(true);
+//                //mWebView.getSettings().setJavaScriptEnabled(true);
+//                //mWebView.loadUrl("http://g5.creditlanka.com/");
+//            }
+//
+//        });
+        //end of event
 
         btn_crtManage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -166,15 +188,36 @@ public class main_menu extends AppCompatActivity {
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+            AlertDialog.Builder altdial= new AlertDialog.Builder(main_menu.this);
+            altdial.setMessage("Are you sure?").setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
 
-                Intent intent = new Intent(main_menu.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
+                        Intent intent = new Intent(main_menu.this, LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+
+            AlertDialog alert= altdial.create();
+            alert.show();
+
+//                Intent intent = new Intent(main_menu.this, LoginActivity.class);
+//                startActivity(intent);
+//                finish();
                 //System.exit(0);
-
 
             }
         });
+
 
 //        my_profile.setOnClickListener(new View.OnClickListener() {
 //        @Override
